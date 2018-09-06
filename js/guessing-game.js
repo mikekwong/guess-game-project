b7f58c957665e9c44f22b6cb4e1ce27f5a2882c2
Mike Wong <wongmdesign@gmail.com>
2018-09-06 13:27:52 -0400

    updated commit

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
 by running "testem".
@@ -7,3 +7,23 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+
+const generateWinningNumber = () => {
+  return Math.floor(Math.random() * 100) + 1;
+};
+
+const shuffle = array => {
+  let m = array.length,
+    t,
+    i;
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
